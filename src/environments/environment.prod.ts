a31a30cff3b94cf3e98e52c1c4cee10c7ba06ccc
MaxonR32 <64862113+MaxonR32@users.noreply.github.com>
2021-09-23 13:17:45 +0300

    change firebase config in env prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyAhywARXG7BjpkKmSrCble_2iV7sYeg0ao",
+    authDomain: "type-racer-3ccd8.firebaseapp.com",
+    projectId: "type-racer-3ccd8",
+    storageBucket: "type-racer-3ccd8.appspot.com",
+    messagingSenderId: "82744933720",
+    appId: "1:82744933720:web:344be5bfa213454e0df92d",
+    dataBaseUrl: 'https://type-racer-3ccd8-default-rtdb.firebaseio.com/'
+  }
 };
